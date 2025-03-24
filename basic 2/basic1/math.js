75aade54047da5a8b00855f2df1c3a71cfdab66e
Vivek56y <vahi7315@gmail.com>
2025-03-24 23:55:31 +0530

    jj

--- basic 2/basic1/math.js
+++ basic 2/basic1/math.js
@@ -1,12 +1,12 @@
-// console.log(Math.abs(-4));
-// console.log(Math.round(4.55))
-// console.log(Math.floor(4.55))
-// console.log(Math.min(1,5,4,6))
-// console.log(Math.max(1,5,4,6))
+console.log(Math.abs(-4));
+console.log(Math.round(4.55))
+console.log(Math.floor(4.55))
+console.log(Math.min(1,5,4,6))
+console.log(Math.max(1,5,4,6))
 
 
-// console.log(Math.floor(Math.random()*10)+1);
-// console.log(Math.max(Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+console.log(Math.max(Math.random()*10)+1);
 // otp generator
 const max=201265
 const min=0
